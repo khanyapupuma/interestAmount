35d84e8823cc293558bc49ec51cbe07e9c9556ef
khanyapupuma <khalilpupuma@gmail.com>
2024-06-03 16:27:17 +0200

    com

---
+++ script/code.js
@@ -0,0 +1,16 @@
+function calculateInterest(amount, percentage) { 
+    let percentageValue = parseFloat(percentage.replace('%', ''));
+    let interestAmount = (amount * percentageValue) / 100;
+    return interestAmount;
+}
+function calculateAndDisplay() {
+    let amount = document.getElementById('amount').value;
+    let percentage = document.getElementById('percentage').value;
+    if (amount && percentage) {
+        amount = parseFloat(amount);
+        let result = calculateInterest(amount, percentage);
+        document.getElementById('result').textContent = `Interest Amount: R${result.toFixed(2)}`;
+    } else {
+        document.getElementById('result').textContent = 'Please enter valid inputs.';
+    }
+}
